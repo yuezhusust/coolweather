a1f879464946ff0eabe897770a89740992c875ec
yuezhu <40314525@qq.com>
2017-02-24 14:11:57 +0800

    增加类

---
+++ app/src/main/java/com/example/lenovo/coolweather/ChooseAreaFragment.java
@@ -0,0 +1,111 @@
+package com.example.lenovo.coolweather;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.lenovo.coolweather.db.Province;
+import com.example.lenovo.coolweather.utils.LogUtil;
+
+import org.litepal.crud.DataSupport;
+
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChooseAreaFragment extends Fragment {
+    private static final int LEVEL_PROVINCE = 0;
+    private static final int LEVEL_CITY = 1;
+    private static final int LEVEL_COUNTY = 2;
+    private int currentLevel;
+
+    private TextView titleText;
+    private Button backButton;
+    private ListView listView;
+    private ArrayAdapter <String> adapter;
+    private List<String> dataList;
+
+    private List<Province> provinceList;
+    private Province selectedProvince;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.choose_area,container,false);
+        titleText = (TextView) view.findViewById(R.id.title_text);
+        backButton = (Button) view.findViewById(R.id.back_button);
+        listView = (ListView) view.findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,dataList);
+        listView.setAdapter(adapter);
+        return view;
+    }
+
+    /**
+     * Called when the fragment's activity has been created and this
+     * fragment's view hierarchy instantiated.  It can be used to do final
+     * initialization once these pieces are in place, such as retrieving
+     * views or restoring state.  It is also useful for fragments that use
+     * {@link #setRetainInstance(boolean)} to retain their instance,
+     * as this callback tells the fragment when it is fully associated with
+     * the new activity instance.  This is called after {@link #onCreateView}
+     * and before {@link #onViewStateRestored(Bundle)}.
+     *
+     * @param savedInstanceState If the fragment is being re-created from
+     *                           a previous saved state, this is the state.
+     */
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+               if (currentLevel == LEVEL_PROVINCE){
+                   selectedProvince = provinceList.get(i);
+                   queryCities();
+               }
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        queryProvinces();
+    }
+
+    private void queryProvinces() {
+        titleText.setText("中国");
+        backButton.setVisibility(View.GONE);
+        provinceList = DataSupport.findAll(Province.class);
+        if (provinceList.size() > 0){
+            dataList.clear();
+            for(Province province:provinceList){
+                dataList.add(province.getProvinceName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_PROVINCE;
+        }else {
+            String address = "http://guolin.tech/api/china";
+            queryFromServer(address,"province");
+        }
+    }
+
+    private void queryFromServer(String address, String type) {
+        LogUtil.
+    }
+
+    private void queryCities() {
+
+    }
+}
--- app/src/main/java/com/example/lenovo/coolweather/utils/LogUtil.java
+++ app/src/main/java/com/example/lenovo/coolweather/utils/LogUtil.java
@@ -1,5 +1,7 @@
 package com.example.lenovo.coolweather.utils;
 
+import android.util.Log;
+
 /**
  * 项目名称：CoolWeather
  * 类描述：
@@ -11,4 +13,40 @@ package com.example.lenovo.coolweather.utils;
  */
 
 public class LogUtil {
+    private static final int LEVEL_VER = 0;
+    private static final int LEVEL_DEBUG = 1;
+    private static final int LEVEL_INFO = 2;
+    private static final int LEVEL_WARN = 3;
+    private static final int LEVEL_ERROR = 4;
+    private static final int LEVEL_NOTHING = 5;
+    private static int currentLevel = LEVEL_VER;
+    private static final String TAG = "LogUtil";
+
+    public static void v(String msg){
+        if (currentLevel <= LEVEL_VER){
+            Log.v(TAG,msg);
+        }
+    }
+    public static void d(String msg){
+        if (currentLevel <= LEVEL_DEBUG) {
+            Log.d(TAG, msg);
+        }
+    }
+    public static void i(String msg){
+        if (currentLevel <= LEVEL_INFO){
+            Log.i(TAG, msg);
+        }
+    }
+    public static void w(String msg){
+        if (currentLevel <= LEVEL_WARN){
+            Log.w(TAG, msg );
+        }
+    }
+    public static void e(String msg){
+        if (currentLevel <= LEVEL_ERROR){
+            Log.e(TAG, msg );
+        }
+    }
+
+
 }
